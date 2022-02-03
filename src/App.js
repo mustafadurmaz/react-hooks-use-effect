da9e71535e35f7c9bfd32f9bacc4aaa0f3657209
Mustafa Durmaz <mdmz_001@hotmail.com>
2022-02-04 01:12:09 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
   useEffect(() => {
@@ -7,7 +8,12 @@ function App() {
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button>Click Me</button>
+      <DogPics />
+    </div>
+  )
 }
 
 export default App;
